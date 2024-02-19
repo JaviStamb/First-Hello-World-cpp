c0f8af6e5ab5d76854e399dd869fa2856e978057
Javier Stambullian <javier.stambullian@wallawalla.edu>
2024-02-18 16:32:16 -0800

    Segunda rama comiteada pero no entiendo todavia

--- helloworld.cpp
+++ helloworld.cpp
@@ -6,5 +6,6 @@
 int main(){
     std::cout << "hello world" << std::endl;
     std::cout << "Probando una nueva rama llamada creando-rama" << std::endl;
+    std::cout << "Probando una segunda rama para ver coom cambian las cosas llamada creando-rama" << std::endl;
     return 0;    
 }
